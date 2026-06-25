80bceba44910ed6a79f8e2e59a00a537ee28831c
Ackyul <yoshua949237217@gmail.com>
2026-06-24 21:20:59 -0500

    aourum

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ const upload = multer({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
